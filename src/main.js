b1175a05052df4a4e21f1cd1835290c0d838111e
erik-t-irgens <49962295+erik-t-irgens@users.noreply.github.com>
2019-07-08 17:31:42 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Front end logic.
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+
+});
---
+++ src/template.js
@@ -0,0 +1,1 @@
+// Backend logic with exports, functions, constructors, etc.
